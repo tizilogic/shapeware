e7572f0e6f7a78c4667119c9908bd1c4f706ad4c
Tiziano Bettio <tc@tizilogic.com>
2023-07-28 14:51:58 +0200

    Improve kfile

--- kfile.js
+++ kfile.js
@@ -10,11 +10,11 @@ project.addFile('Sources/**');
 project.addIncludeDir('Sources');
 if (typeof noKrinkPlease === 'undefined') {
     await project.addProject('krink');
-    project.addFile('Shaders/**');
-    project.setDebugDir('Deployment');
-    project.addDefine('KINC_NO_WAYLAND');
 }
 
+project.addFile('Shaders/**');
+project.setDebugDir('Deployment');
+project.addDefine('KINC_NO_WAYLAND');
 project.addFile('ext/sht/sht.c');
 project.addFile('ext/sht/murmur3.c');
 project.addIncludeDir('ext');
